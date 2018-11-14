7f4475bab063cf3a8227c2a25775334ed38233cd
nirzamircybr <45031482+nirzamircybr@users.noreply.github.com>
2018-11-14 13:46:44 +0000

    Update send_build_metadata.bat

--- send_build_metadata.bat
+++ send_build_metadata.bat
@@ -3,5 +3,5 @@ echo "GIT_BRANCH %GIT_BRANCH%"
 echo "GIT_URL %GIT_URL%"
 echo "CHANGE_AUTHOR %CHANGE_AUTHOR%"
 
-C:\Program Files\curl\curl-7.62.0-win64-mingw\bin\curl -H "Content-Type: application/json" -X POST http://18.191.200.247/ingress/jenkins -d "{\"Commit\":\"%GIT_COMMIT%\"}"
+"C:\Program Files\curl\curl-7.62.0-win64-mingw\bin\curl" -H "Content-Type: application/json" -X POST http://18.191.200.247/ingress/jenkins -d "{\"Commit\":\"%GIT_COMMIT%\"}"
 
